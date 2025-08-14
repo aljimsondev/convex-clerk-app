a13feb3cdee1e300f811aaa37e09013d68fd9a5a
aljimsondev <aljimson.megrino@gmail.com>
2025-08-14 12:09:40 +0800

    Chore | apply force-cache

--- src/app/exclusive-content/_components/contents.tsx
+++ src/app/exclusive-content/_components/contents.tsx
@@ -8,22 +8,35 @@ export type ExclusiveContentType = {
   imageUrl: string;
 };
 
+async function fetchPosts() {
+  try {
+    const contents = (await fetch(
+      'https://jsonplaceholder.typicode.com/posts?_limit=20',
+      { cache: 'force-cache' },
+    ).then(async (data) => {
+      const posts = await data.json();
+
+      return await Promise.all(
+        posts.map(async (post: ExclusiveContentType, index: number) => {
+          const url = await fetch(
+            `https://picsum.photos/200/300?random=${index}`,
+            { cache: 'force-cache' },
+          ).then(async (response) => await response.url);
+
+          return { ...post, imageUrl: url };
+        }),
+      );
+    })) as ExclusiveContentType[];
+
+    return contents;
+  } catch (e) {
+    console.warn(e);
+    return [];
+  }
+}
+
 async function ExclusiveContents() {
-  const contents = (await fetch(
-    'https://jsonplaceholder.typicode.com/posts?_limit=20',
-  ).then(async (data) => {
-    const posts = await data.json();
-
-    return await Promise.all(
-      posts.map(async (post: ExclusiveContentType, index: number) => {
-        const url = await fetch(
-          `https://picsum.photos/200/300?random=${index}`,
-        ).then(async (response) => await response.url);
-
-        return { ...post, imageUrl: url };
-      }),
-    );
-  })) as ExclusiveContentType[];
+  const contents = await fetchPosts();
 
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-4">
